ede843443106fd42992567845e001d1582c08c23
Gleb Chanov <zebrach77@gmail.com>
2022-06-26 16:55:30 +0300

    Qsort FIXED

--- quick_sort/main.py
+++ quick_sort/main.py
@@ -14,26 +14,18 @@ def find_partition(arr, l, r):
             temp = arr[ind1]
             arr[ind1] = arr[ind2]
             arr[ind2] = temp
-            # if arr[ind1] == base_elem:
-            #     base_elem_ind = ind1
-            # if arr[ind2] == base_elem:
-            #     base_elem_ind = ind2
             ind1 += 1
             ind2 -= 1
 
-    return ind2, arr #???
-
-
-
-
+    return ind2, arr
 
 
 def qsort(arr, l_ind, r_ind):
     if l_ind >= r_ind:
         return arr
-    base, arr = find_partition(arr, l_ind, r_ind)
-    arr = qsort(arr, l_ind, base) #???
-    arr = qsort(arr, base+1, r_ind)
+    edge, arr = find_partition(arr, l_ind, r_ind)
+    arr = qsort(arr, l_ind, edge)
+    arr = qsort(arr, edge + 1, r_ind)
     return arr
 
 
