f04f730f184b10ab6a6e85ccbdbd183a51b2f644
Gleb Chanov <zebrach77@gmail.com>
2022-06-28 19:48:30 +0300

    Bubble Sort FIXED

--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -1,8 +1,8 @@
 def bubble_sort(arr):
+    if len(arr) <= 1:
+        return arr
     for i in range(len(arr)):
-        if len(arr) <= 1:
-            return arr
-        ind = 1
+        ind = i-1 if i > 1 else 1
         while ind < len(arr):
             if arr[ind-1] > arr[ind]:
                 temp = arr[ind-1]
@@ -10,6 +10,8 @@ def bubble_sort(arr):
                 arr[ind] = temp
             ind += 1
     return arr
+
+
 a = [int(i) for i in input().split()]
 print(bubble_sort(a))
 
