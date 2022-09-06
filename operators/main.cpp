cee968f0ebddd9eb91c7f13bd6d94a3773088e37
zebrach77 <zebrach77@gmail.com>
2022-09-06 18:33:41 +0300

    big numbers done

--- operators/main.cpp
+++ operators/main.cpp
@@ -74,27 +74,27 @@ private:
 	}
 	std::string SUB(std::string s1, std::string s2, size_t added_num) {
 		if (s1.empty() && s2.empty()) {
-			return (added_num)?  "-" : "";
+			return "0";
 		}
 		if (s1.empty()) {
-			int b = s2.back() - '0' - added_num;
-			size_t a = (b >= 0)? 0 : 1;
-			b = (b >= 0)? b : (b+10)/10;
+			int b = s2.back() - '0' - added_num + 10;
+			size_t a = (b < 10)? 1 : 0;
+			b = b % 10;
 			s2.pop_back();
 			std::string temp = SUB(s1, s2, a);
 			return (temp == "0")? std::to_string(b): temp + std::to_string(b);
 		}
 		if (s2.empty()) {
-			int b = s1.back() - '0' - added_num;
-			size_t a = (b >= 0)? 0 : 1;
-			b = (b >= 0)? b : (b+10)/10;
+			int b = s1.back() - '0' - added_num + 10;
+			size_t a = (b < 10)? 1 : 0;
+			b = b % 10;
 			s1.pop_back();
 			std::string temp = SUB(s1, s2, a);
 			return (temp == "0")? std::to_string(b): temp + std::to_string(b);
 		}
-		int b = s1.back() - '0' - added_num - (s2.back() - '0');
-		size_t a = (b >= 0)? 0 : 1;
-		b = (b>=0)? b : (b+10)/10;
+		int b = s1.back() - '0' - added_num - (s2.back() - '0') + 10;
+		size_t a = (b < 10)? 1 : 0;
+		b = b % 10;
 		s2.pop_back();
 		s1.pop_back();
 		std::string temp = SUB(s1, s2, a);
@@ -105,6 +105,9 @@ private:
 		if (s1.length() == 1 && s2.length() == 1) {
 			return std::to_string(std::stoi(s1)*std::stoi(s2));
 		}
+		if (s1 == "0" or s2 == "0") {
+			return "0";
+		}
 		std::string s1_1;
 		std::string s1_2;
 		std::string s2_1;
@@ -114,46 +117,56 @@ private:
 		std::string t3;
 		std::string t4;
 		std::string t5;
+		size_t l1 = s1.length();
+		size_t l2 = s2.length();
+		size_t l = (std::max(l1, l2) + 1) / 2;
+		size_t c1 = 0;
+		size_t c2 = 0;
+		l *= 2;
+		for (size_t i = l1; i < l; ++i) {
+			s1 += "0";
+			++c1;
+		}
+		for (size_t i = l2; i < l; ++i) {
+			s2 += "0";
+			++c2;
+		}
 
-		if (s1.length() % 2 == 0) {
-			 s1_1 = s1.substr(0, s1.length()/2);
-			 s1_2 = s1.substr(s1.length()/2, s1.length()/2);
-		}
-		else {
-			s1_1 = s1.substr(0, s1.length()/2);
-			s1_2 = s1.substr(s1.length()/2, s1.length()/2+1);
-		}
 
 
-		if (s2.length() % 2 == 0) {
-			s2_1 = s2.substr(0, s2.length()/2);
-			s2_2 = s2.substr(s2.length()/2, s2.length());
+
+
+		s1_1 = s1.substr(0, s1.length()/2);
+		s1_2 = s1.substr(s1.length()/2, s1.length()/2);
+		while (s1_2.substr(0, 1) == "0" && s1_2.length() > 1) {
+			s1_2 = s1_2.substr(1);
 		}
-		else {
-			s2_1 = s2.substr(0, s2.length()/2);
-			s2_2 = s2.substr(s2.length()/2, s2.length()+1);
+
+		s2_1 = s2.substr(0, s2.length()/2);
+		s2_2 = s2.substr(s2.length()/2, s2.length());
+		while (s2_2.substr(0, 1) == "0" && s2_2.length() > 1) {
+			s2_2 = s2_2.substr(1);
 		}
-		if (s1.length() == 1) {
-			t1 = MUL(s1, ADD(s2_1, s2_2, 0));
-			t3 = MUL(s1, s2_2);
-			t5 = SUB(t1, t3, 0);
-			t5 += (t5=="0")? "" : "0";
-			return ADD(t3, t5, 0);
-		}
-		if (s2.length() == 1) {
-			t1 = MUL(ADD(s1_1, s1_2, 0), s2);
-			t3 = MUL(s1_2, s2);
-			t5 = SUB(t1, t3, 0);
-			t5 += (t5=="0")? "" : "0";
-			return ADD(t3, t5, 0);
-		}
+
 		t1 = MUL(ADD(s1_1, s1_2, 0), ADD(s2_1, s2_2, 0));
 		t2 = MUL(s1_1, s2_1);
 		t3 = MUL(s1_2, s2_2);
-		t4 = (t2 == "0")? "0" : t2 + "00";
+		std::string zeros1 = "";
+		std::string zeros2 = "";
+		for (int i = 0; i < l/2; ++i) {
+			zeros1 += "0";
+			zeros2 += "00";
+		}
+		t4 = (t2 == "0")? "0" : t2 + zeros2;
 		t5 = SUB(SUB(t1, t2, 0), t3, 0);
-		t5 += (t5=="0")? "" : "0";
-		return ADD(ADD(t3, t5, 0), t4, 0);
+		t5 = (t5 == "0")? t5 : t5 + zeros1;
+		std::string tfin = ADD(ADD(t3, t5, 0), t4, 0);
+		for (size_t i = 0; i < c1+c2; ++i) {
+			if (tfin.back()-'0' == 0) {
+				tfin.pop_back();
+			}
+		}
+		return tfin;
 	}
 };
 
