24050348c956938cf35763aed33b18b4c53b2f96
zebrach77 <zebrach77@gmail.com>
2022-10-13 19:04:02 +0300

    queue spinlock in process

--- ticket_lock/main.cpp
+++ ticket_lock/main.cpp
@@ -8,9 +8,51 @@ struct Node {
 	size_t ticket_number;
 };
 
-class QueueSpinLock {
-public:
-private:
-	Node* head_ = new Node(0);
-	Node* tail_ = head_;
-};
+#pragma once
+
+namespace spinlocks {
+
+/*  Scalable Queue SpinLock
+ *
+ *  Usage:
+ *
+ *  QueueSpinLock qspinlock;
+ *  {
+ *    QueueSpinLock::Guard guard(qspinlock);  // <-- Acquire
+ *    // Critical section
+ *  }  // <-- Release
+ */
+
+	class QueueSpinLock {
+	public:
+		class Guard {
+			friend class QueueSpinLock;
+
+		public:
+			explicit Guard(QueueSpinLock& spinlock) : spinlock_(spinlock) {
+				spinlock_.Acquire(this);
+			}
+
+			~Guard() {
+				spinlock_.Release(this);
+			}
+
+		private:
+			QueueSpinLock& spinlock_;
+			// ???
+		};
+
+	private:
+		void Acquire(Guard* /*guard*/) {
+			// Your code goes here
+		}
+
+		void Release(Guard* /*owner*/) {
+			// Your code goes here
+		}
+
+	private:
+		// ???
+	};
+
+}  // namespace spinlocks
